982dff6eb5eef7fe7b32f6c7c118f6bfa10cc2c6
Sowrov <mdrasheduli521@gmail.com>
2024-03-21 14:43:37 +0600

    gile.py

--- gile.py
+++ gile.py
@@ -1,1 +1,185 @@
-print(45+57)
+class file:
+    def __init__(self, file1, file2):
+        self.file1 = file1
+        self.file2 = file2
+
+    def recive(self):
+        print("file is run:", self.file1)
+
+
+if __name__ == "__main__":
+    file("hello file", "exjeckley file")
+    v = file
+
+import turtle
+
+turtle.shape("turtle")
+turtle.color("red")
+for i in range(5):
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.backward(50)
+    turtle.right(90)
+turtle.exitonclick()
+
+# driving
+import pygame
+import sys
+import random
+
+# Constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+CAR_WIDTH = 50
+CAR_HEIGHT = 100
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+
+
+# Car class
+class Car:
+    def __init__(self):
+        self.width = CAR_WIDTH
+        self.height = CAR_HEIGHT
+        self.x = SCREEN_WIDTH // 2 - self.width // 2
+        self.y = SCREEN_HEIGHT - self.height - 20
+        self.speed = 5
+
+    def move_left(self):
+        self.x -= self.speed
+
+    def move_right(self):
+        self.x += self.speed
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, RED, (self.x, self.y, self.width, self.height))
+
+
+# Obstacle class
+class Obstacle:
+    def __init__(self):
+        self.width = random.randint(50, 200)
+        self.height = 20
+        self.x = random.randint(0, SCREEN_WIDTH - self.width)
+        self.y = -self.height
+        self.speed = random.randint(2, 5)
+
+    def move(self):
+        self.y += self.speed
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, BLACK, (self.x, self.y, self.width, self.height))
+
+
+# Main function
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Self Driving Car")
+
+    clock = pygame.time.Clock()
+
+    car = Car()
+    obstacles = []
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            car.move_left()
+        if keys[pygame.K_RIGHT]:
+            car.move_right()
+
+        screen.fill(WHITE)
+
+        # Generate obstacles
+        if random.randint(0, 100) < 3:
+            obstacle = Obstacle()
+            obstacles.append(obstacle)
+
+        # Move and draw obstacles
+        for obstacle in obstacles:
+            obstacle.move()
+            obstacle.draw(screen)
+
+            if obstacle.y > SCREEN_HEIGHT:
+                obstacles.remove(obstacle)
+
+        car.draw(screen)
+
+        pygame.display.flip()
+        clock.tick(60)
+
+
+if __name__ == "__main__":
+    main()
+
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode((1200, 400))
+track = pygame.image.load('track6.png')
+car = pygame.image.load('tesla.png')
+car = pygame.transform.scale(car, (30, 60))
+car_x = 155
+car_y = 300
+focal_dis = 25
+cam_x_offset = 0
+cam_y_offset = 0
+direction = 'up'
+drive = True
+clock = pygame.time.Clock()
+while drive:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            drive = False
+    clock.tick(60)
+    cam_x = car_x + cam_x_offset + 15
+    cam_y = car_y + cam_y_offset + 15
+    up_px = window.get_at((cam_x, cam_y - focal_dis))[0]
+    down_px = window.get_at((cam_x, cam_y + focal_dis))[0]
+    right_px = window.get_at((cam_x + focal_dis, cam_y))[0]
+    print(up_px, right_px, down_px)
+
+    # change direction (take turn)
+    if direction == 'up' and up_px != 255 and right_px == 255:
+        direction = 'right'
+        cam_x_offset = 30
+        car = pygame.transform.rotate(car, -90)
+    elif direction == 'right' and right_px != 255 and down_px == 255:
+        direction = 'down'
+        car_x = car_x + 30
+        cam_x_offset = 0
+        cam_y_offset = 30
+        car = pygame.transform.rotate(car, -90)
+    elif direction == 'down' and down_px != 255 and right_px == 255:
+        direction = 'right'
+        car_y = car_y + 30
+        cam_x_offset = 30
+        cam_y_offset = 0
+        car = pygame.transform.rotate(car, 90)
+    elif direction == 'right' and right_px != 255 and up_px == 255:
+        direction = 'up'
+        car_x = car_x + 30
+        cam_x_offset = 0
+        car = pygame.transform.rotate(car, 90)
+    # drive
+    if direction == 'up' and up_px == 255:
+        car_y = car_y - 2
+    elif direction == 'right' and right_px == 255:
+        car_x = car_x + 2
+    elif direction == 'down' and down_px == 255:
+        car_y = car_y + 2
+    window.blit(track, (0, 0))
+    window.blit(car, (car_x, car_y))
+    pygame.draw.circle(window, (0, 255, 0), (cam_x, cam_y), 5, 5)
+    pygame.display.update()
+
+
+
+
